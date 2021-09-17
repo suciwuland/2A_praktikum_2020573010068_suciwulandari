38b9afd73fe45c092b5f6b6f772c784b50314321
suciwuland <suciwd3@gmail.com>
2021-09-18 01:06:59 +0700

    ubah skrip

---
+++ Praktikum2/coba3-variabel2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $jalan="Medan-B.Aceh";
+        $noRumah=29;
+        $blok="$C";
+        $jumlahPenghuni=3;
+        echo "Hasil Variabel variabel jalan + variabel noRumah adalah ";
+        echo $hasil=$noRumah+jumlahPenghuni;
+        echo "<br>";
+        
+    ?>
+</body>
+</html>
