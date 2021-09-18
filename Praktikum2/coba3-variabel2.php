de240f058140ee65aefecd16d8b9c9495d54937f
suciwuland <suciwd3@gmail.com>
2021-09-18 13:14:01 +0700

    menyelesaikan sintak coba var3

--- Praktikum2/coba3-variabel2.php
+++ Praktikum2/coba3-variabel2.php
@@ -15,7 +15,9 @@
         echo "Hasil Variabel variabel jalan + variabel noRumah adalah ";
         echo $hasil=$noRumah+jumlahPenghuni;
         echo "<br>";
-        
+        echo "Hasil Variabel variabel blok + variabel noRumah adalah ";
+        echo $hasil= $blok + $noRumah;
+        echo "<br>";
     ?>
 </body>
 </html>
