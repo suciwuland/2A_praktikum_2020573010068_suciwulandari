56c02ea8d4d27e5f9f14fb805797d4ea6620a4e9
suciwuland <suciwd3@gmail.com>
2021-09-18 13:39:10 +0700

    buat penugasan operator

--- Praktikum2/coba3-variabel2.php
+++ Praktikum2/coba3-variabel2.php
@@ -12,13 +12,16 @@
         $noRumah=29;
         $blok="4C";
         $jumlahPenghuni= 3;
-        echo "Hasil Variabel variabel jalan + variabel noRumah adalah ";
+        echo "Hasil Variabel variabel jalan + variabel noRumah 
+        adalah ";
         echo $alamat= $jalan . $noRumah;
         echo "<br>";
-        echo "Hasil Variabel variabel norumah + variabel jumlahPenghuni adalah ";
+        echo "Hasil Variabel variabel norumah + variabel jumlahPenghuni 
+        adalah ";
         echo $hasil=$noRumah + $jumlahPenghuni;
         echo "<br>";
-        echo "Hasil Variabel variabel blok + variabel noRumah adalah ";
+        echo "Hasil Variabel variabel blok + variabel noRumah
+         adalah ";
         echo $hasil = $blok . $noRumah;
         echo "<br>";
     ?>
