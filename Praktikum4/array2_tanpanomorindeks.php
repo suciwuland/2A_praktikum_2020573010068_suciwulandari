b3f0fed53a39142fc7dd548391437efa5d85d0e9
suciwuland <suciwd3@gmail.com>
2021-10-01 15:10:35 +0700

    prak4

--- Praktikum4/array2_tanpanomorindeks.php
+++ Praktikum4/array2_tanpanomorindeks.php
@@ -13,8 +13,8 @@
         $nama[]="Zakiuddin";
         echo "Hallo";
         echo "Isi array \$anak[0] adalah $nama[0]"; echo "<br>";
-        echo "Isi array \$anak[0] adalah $nama[1]"; echo "<br>";
-        echo "Isi array \$anak[0] adalah $nama[2]"; echo "<br>";
+        echo "Isi array \$anak[1] adalah $nama[1]"; echo "<br>";
+        echo "Isi array \$anak[2] adalah $nama[2]"; echo "<br>";
     ?>
 </body>
 </html>
