d9a436a6f0f5c1467d7f9f75700352f9de70642c
suciwuland <suciwd3@gmail.com>
2021-10-05 12:28:53 +0700

    praktikum5

---
+++ Praktikum5/bandingnilai.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contoh Form dengan POST</title>
+</head>
+<body>
+    <form action="pro_post_nilai.php" method="post">
+        Bil I :
+        <input type="text" name="bil1">
+        <br><br>
+        Bil II:
+        <input type="text" name="bil2">
+        <br><br>
+        <input type="submit" value="bandingkan">
+    </form>
+</body>
+</html>
---
+++ Praktikum5/methodgetproses.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Method GET Proses</title>
+</head>
+<body>
+    Nama yang anda inputkan adalah:<?php
+    echo $_GET["nama"];?>
+
+</body>
+</html>
