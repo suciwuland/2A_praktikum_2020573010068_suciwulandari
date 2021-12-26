f17d7bd2a6806f786be4137e953bd842ab316d8d
suciwuland <suciwd3@gmail.com>
2021-12-26 19:47:44 +0700

    project

--- Project/profile.php
+++ Project/profile.php
@@ -1,6 +1,6 @@
 <?php
 require "proses/session.php";
-$sql = mysqli_query($conn, "SELECT * FROM tbuser usr LEFT JOIN tbprofile pro ON usr.id_user=pro.id_user WHERE  username='$_SESSION[username]' ");
+$sql = mysqli_query($conn, "SELECT * FROM tbuser usr LEFT JOIN tbprofile pro ON usr.id_user=pro.user WHERE username='$_SESSION[username]' ");
 $data = mysqli_fetch_array($sql);
 ?>
 <!DOCTYPE html>
@@ -14,7 +14,7 @@ $data = mysqli_fetch_array($sql);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="/assets/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="css/sidebars.css" rel="stylesheet">
-    <title>SICAS SISTEM INFORMASI</title>
+    <title>Profile</title>
 </head>
 
 <body>
